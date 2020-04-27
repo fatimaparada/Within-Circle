70f4ddb1dc3eb0d32f2dc7edc978e51fe57b7b36
Fatima Parada-Taboada <55894952+fatimaparada@users.noreply.github.com>
2020-04-26 19:46:44 -0500

    Add files via upload

---
+++ Exercise3_22.java
@@ -0,0 +1,32 @@
+
+/*
+Author: Fatima Parada-Taboada
+Net id: fnp170130
+Program overview: This program will determine whether a point chosen by the user is within the radius of a circle.
+*/ 
+
+import java.util.Scanner;
+
+public class Exercise03_22 {
+	public static void main (String [] args) {
+		Scanner input = new Scanner(System.in);
+		double x, y;
+		double distance;
+
+	    System.out.printLn("Enter a point with two coordinates: ");
+	    x = input.nextDouble;
+	    y = input.nextDouble;
+
+	    distance = Math.pow(x * x + y * y, 0.5);
+	
+	    if (distance < 10) {
+		    System.out.printLn("Point (" + x + ", " + y + ") is in the circle");
+
+		    System.out.printf("Point (%.2f, %.2f) is in the circle\n", x, y);
+	    }
+	    else {
+		System.out.printLn("Point (" + x + ", " + y + ") is not in the circle");
+	    }
+    
+    }
+}
